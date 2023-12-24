cb711e8e54dce026cfef5cf8084394dcad0d7390
ratan <ratan@codewithratan.com>
2023-12-24 13:34:08 +0530

    conversion of data

---
+++ javascript/03-conversion&oper.js
@@ -0,0 +1,20 @@
+// let score="ratan";
+// console.log(typeof score);
+// console.log(typeof score);
+
+
+// // typecasting
+// let scoreInNumber=Number(score);
+// console.log(typeof scoreInNumber);
+
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+// let convertInString=String(score);
+// console.log( typeof convertInString);
+// console.log(convertInString);
+let isLoggedIn="ratan";
+console.log(isLoggedIn);
+
+let convertInBoolean=Boolean(isLoggedIn);
+console.log(convertInBoolean);
